f654f5e5ad2eeb93a870b1c1285c8d379132ab7e
김유섭 <dbtjq1592000@gmail.com>
2023-07-17 16:33:12 +0900

    Revert "Add user entity"
    This reverts commit 90cd6e96ff5bbd758b06961fbc6d7bb9dfe44929

--- 0. Project 심다/simda/src/main/java/ssafy/a709/domain/User.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/domain/User.java
@@ -1,10 +1,8 @@
 package ssafy.a709.domain;
 
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,35 +10,23 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class User {
 
-    // User Id
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "int")
+    @Id @GeneratedValue
+    @Column
     private int userId;
 
-    // Social Token
-    @Column(columnDefinition = "varchar(200)", unique=true)
-    @NotNull
+    @Column
     private String socialToken;
 
-    // Social Type
-    @Column(columnDefinition = "varchar(50)")
-    @NotNull
+    @Column
     private String socialType;
 
-    // Nickname
-    @Column(columnDefinition = "varchar(50)", unique=true)
-    @NotNull
+    @Column
     private String nickname;
 
-    // Profile Img Address
-    @Column(columnDefinition = "varchar(100)")
+    @Column
     private String profileImg;
 
-    // userRole
     @Column
     @Enumerated(EnumType.ORDINAL)
-    @NotNull
-    @ColumnDefault("2")
-    private UserRole userRole;
+    private UserRole useRole;
 }
