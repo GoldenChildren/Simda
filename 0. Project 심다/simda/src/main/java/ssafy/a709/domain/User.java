90cd6e96ff5bbd758b06961fbc6d7bb9dfe44929
unknown <dbtjq1592000@gmail.com>
2023-07-17 16:26:44 +0900

    Add user entity

--- 0. Project 심다/simda/src/main/java/ssafy/a709/domain/User.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/domain/User.java
@@ -1,8 +1,10 @@
 package ssafy.a709.domain;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,23 +12,35 @@ import javax.persistence.*;
 @Entity
 public class User {
 
-    @Id @GeneratedValue
-    @Column
+    // User Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "int")
     private int userId;
 
-    @Column
+    // Social Token
+    @Column(columnDefinition = "varchar(200)", unique=true)
+    @NotNull
     private String socialToken;
 
-    @Column
+    // Social Type
+    @Column(columnDefinition = "varchar(50)")
+    @NotNull
     private String socialType;
 
-    @Column
+    // Nickname
+    @Column(columnDefinition = "varchar(50)", unique=true)
+    @NotNull
     private String nickname;
 
-    @Column
+    // Profile Img Address
+    @Column(columnDefinition = "varchar(100)")
     private String profileImg;
 
+    // userRole
     @Column
     @Enumerated(EnumType.ORDINAL)
-    private UserRole useRole;
+    @NotNull
+    @ColumnDefault("2")
+    private UserRole userRole;
 }
