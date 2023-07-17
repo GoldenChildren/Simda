7ca8b7062094482f247549145682f443cd567437
jht043 <jht043@naver.com>
2023-07-17 14:24:23 +0900

    07-17 피드/코멘트 엔티티 base(FK설정해야됨)

--- 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Feed.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Feed.java
@@ -1,4 +1,32 @@
 package ssafy.a709.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int feedId;
+
+    private int userId;
+
+    private int emotion;
+    private String content;
+    private String img;
+    private double lat;
+    private double lng;
+    private int like;
+    private String regDate;
+
 }
