3e4454e4eae118062bd51ad4a550c7463c047b32
KimByeongChan <qudcks8749@naver.com>
2023-07-17 14:13:48 +0900

    필드 추가

--- 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Chat.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Chat.java
@@ -1,4 +1,31 @@
 package ssafy.a709.domain;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Date;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Chat {
+    @Id
+    @GeneratedValue
+    private int chatId;
+
+    private int roomId;
+
+    private int userId;
+
+    private String content;
+
+    private Timestamp regDate;
+
+    private boolean read;
 }
--- 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Chatroom.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/domain/Chatroom.java
@@ -1,4 +1,25 @@
 package ssafy.a709.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Chatroom {
+
+    @Id
+    private int roomId;
+
+    private int user1Id;
+
+    private int user2Id;
+    @Column(name = "l_chat_id")
+    private int lChatId;
 }
