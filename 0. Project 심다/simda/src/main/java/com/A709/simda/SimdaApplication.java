75b6c265f0cc3618493af7d3f2b3d394b6ac39a7
unknown <dbtjq1592000@gmail.com>
2023-07-17 09:35:57 +0900

    Spring

---
+++ 0. Project 심다/simda/src/main/java/com/A709/simda/SimdaApplication.java
@@ -0,0 +1,13 @@
+package com.A709.simda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimdaApplication.class, args);
+	}
+
+}
---
+++ 0. Project 심다/simda/src/test/java/com/A709/simda/SimdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.A709.simda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
