c58d79fde624e8d0906f2c0ff5033edd18e5623c
jht043 <jht043@naver.com>
2023-07-20 12:51:50 +0900

    7/20 FeedAPI 완료

--- 0. Project 심다/simda/src/main/java/ssafy/a709/simda/dto/FeedDto.java
+++ 0. Project 심다/simda/src/main/java/ssafy/a709/simda/dto/FeedDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ssafy.a709.simda.domain.Feed;
+import ssafy.a709.simda.domain.User;
 
 import java.sql.Timestamp;
 
@@ -28,22 +29,22 @@ public class FeedDto {
     // Longitude
     private double lng;
     // Linke Point
-    private int like;
+    private int likeCnt;
     // Regist Date
     private Timestamp regDate;
 
     // Feed(Entity)를 FeedDto로 변환
-//    public static FeedDto changeToFeedDto(Feed feed){
-//        return FeedDto.builder()
-//                .feedId(feed.getFeedId())
-////                .userDto(UserDto.changeToUserDto(feed.getUser()))
-//                .emotion(feed.getEmotion())
-//                .content(feed.getContent())
-//                .img(feed.getImg())
-//                .lat(feed.getLat())
-//                .lng(feed.getLng())
-//                .like(feed.getLike())
-//                .regDate(feed.getRegDate())
-//                .build();
-//    }
+    public static FeedDto changeToFeedDto(Feed feed, User user){
+        return FeedDto.builder()
+                .feedId(feed.getFeedId())
+                .userDto(UserDto.changeToUserDto(user))
+                .emotion(feed.getEmotion())
+                .content(feed.getContent())
+                .img(feed.getImg())
+                .lat(feed.getLat())
+                .lng(feed.getLng())
+                .likeCnt(feed.getLikeCnt())
+                .regDate(feed.getRegDate())
+                .build();
+    }
 }
