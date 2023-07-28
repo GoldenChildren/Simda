80b739e76282fc282f10b5ad71ca58957a3ffa0e
YS <dbtjq1592000@gmail.com>
2023-07-28 16:39:13 +0900

    google bard add

--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/api/BardApi.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/api/BardApi.java
@@ -8,9 +8,10 @@ import java.nio.charset.StandardCharsets;
 public class BardApi {
     public static void main(String[] args) throws IOException {
         ProcessBuilder pb = new ProcessBuilder("C:\\Users\\SSAFY\\AppData\\Local\\Programs\\Python\\Python311\\python",
-                "C:\\Users\\SSAFY\\Desktop\\Project\\S09P12A709\\ProjectSimda\\simda\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py", "아이고");
+                "C:\\Users\\SSAFY\\Desktop\\Project\\S09P12A709\\ProjectSimda\\simda\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
+                "아이고");
         Process p = pb.start();
-        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "euc-kr"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "utf-8"));
 
         String line = " ";
         String output = "";
@@ -19,11 +20,19 @@ public class BardApi {
             line = br.readLine();
             output += "\n";
         }
-
-
-
-        if(output.contains("4")) {
-            System.out.println("hello, number 4");
+        // 0 : 행복, 1 : 기쁨, 2 : 평온, 3 : 화남, 4 : 슬픔
+        if(output.contains("0")) {
+            System.out.println("hello, 0 행복");
+        } else if(output.contains("1")) {
+            System.out.println("hello, 1 기쁨");
+        } else if(output.contains("2")) {
+            System.out.println("hello, 2 평온");
+        } else if(output.contains("3")) {
+            System.out.println("hello, 3 화남");
+        } else if(output.contains("4")) {
+            System.out.println("hello, 4 슬픔");
+        } else {
+            System.out.println("변환 실패, 기본 값으로 설정");
         }
     }
 }
--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/api/bard.py
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/api/bard.py
@@ -1,6 +1,10 @@
 import sys
 import os
+import io
 import bardapi
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
 
 workingdirectory = os.getcwd()
 
@@ -10,9 +14,9 @@ def addition():
 # 질문 작성
 
     input_text = sys.argv[1]
-    # input_text = "슬퍼용"
+    # input_text = "좋아용"
     
-    input_tailInfo = "(이)라는 문장은 다음 보기 중 어디에 가장 가까워? 1번:행복, 2번:평온, 3번:기쁨, 4번:슬픔, 5번:화남. 대답은 다음과 같은 형식으로만 대답해. 예시) 1번"
+    input_tailInfo = "(이)라는 문장은 다음 보기 중 어디에 가장 가까워? 0 : 행복, 1 : 기쁨, 2 : 평온, 3 : 화남, 4 : 슬픔. 대답은 다음과 같은 형식으로만 대답해. ex) 답 : 1"
     
     # 영어 버전
     # input_text = sys.argv[1] + "Which of the following examples is the closest to the sentence (this)? Number 1: Happiness, Number 2: Calm, Number 3: Joy, Number 4: Sad, Number 5: Angry. Answer only in the following format. Example: Number 1"
