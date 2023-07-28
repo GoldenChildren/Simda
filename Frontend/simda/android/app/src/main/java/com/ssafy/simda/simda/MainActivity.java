2ddabd89076f5bd4183a71f1fdab352eb8d5b439
김병찬 <qudcks8749@naver.com>
2023-07-28 16:17:10 +0900

    Frontend 큰 놈 삭제

--- Frontend/simda/android/app/src/main/java/com/ssafy/simda/simda/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.simda.simda;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Frontend/simda/lib/KakaoLogin/social_login.dart
+++
@@ -1,5 +0,0 @@
-abstract class SocialLogin {
-  Future<bool> login();
-  Future<bool> logout();
-  Future<bool> signup(String path, String nickname);
-}
