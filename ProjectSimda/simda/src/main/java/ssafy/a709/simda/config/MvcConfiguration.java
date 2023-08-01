bab9f90f9d24859dd3d1fbc95c0c3561b2226b7d
KimByeongChan <qudcks8749@naver.com>
2023-08-01 10:31:17 +0900

    스웨거 안되던거 수정

--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/config/MvcConfiguration.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/config/MvcConfiguration.java
@@ -10,9 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("swagger-ui.html")
-                        .addResourceLocations("classpath:/META-INF/resources/");
-
+        registry.addResourceHandler("/swagger-ui.html**")
+                        .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/img/profile/**").addResourceLocations("classpath:/static/img/profile/").setCachePeriod(60*60*24*365);
     }
 }
