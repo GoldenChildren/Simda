e6dcd419f20f3eeabfe0de66d8e1d10b7decbd11
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-08-02 15:08:02 +0900

    달력 꽃모양 마커 완료_DB 용 수정 필요

--- frontend/simda/lib/calendar.dart
+++ frontend/simda/lib/calendar.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
-class TableCalendarScreen extends StatelessWidget {
+class TableCalendarScreen extends StatefulWidget {
   const TableCalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  _TableCalendarScreenState createState() => _TableCalendarScreenState();
+}
+
+class _TableCalendarScreenState extends State<TableCalendarScreen> {
+  late final Image _flowerPink;
+  late final Image _flowerGreen;
+  late final Image _flowerPurple;
+  late final Image _flowerBlue;
+  late final Image _flowerYellow;
+
+  final Map<DateTime, List<Widget>> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _flowerPink = Image.asset('assets/images/flowerPink.png', width: 24, height: 24);
+    _flowerGreen = Image.asset('assets/images/flowerGreen.png', width: 24, height: 24);
+    _flowerPurple = Image.asset('assets/images/flowerPurple.png', width: 24, height: 24);
+    _flowerBlue = Image.asset('assets/images/flowerBlue.png', width: 24, height: 24,);
+    _flowerYellow = Image.asset('assets/images/flowerYellow.png', width: 24, height: 24,);
+
+
+    _markers.addAll({
+      DateTime.utc(2023, 7, 1): [_flowerPink],
+      DateTime.utc(2023, 7, 3): [_flowerGreen],
+      DateTime.utc(2023, 7, 4): [_flowerGreen],
+      DateTime.utc(2023, 7, 5): [_flowerBlue],
+      DateTime.utc(2023, 7, 6): [_flowerGreen],
+      DateTime.utc(2023, 7, 9): [_flowerBlue],
+      DateTime.utc(2023, 7, 11): [_flowerGreen],
+      DateTime.utc(2023, 7, 14): [_flowerPurple],
+      DateTime.utc(2023, 7, 15): [_flowerYellow],
+      DateTime.utc(2023, 7, 16): [_flowerYellow],
+      DateTime.utc(2023, 7, 18): [_flowerYellow],
+      DateTime.utc(2023, 7, 20): [_flowerPurple],
+      DateTime.utc(2023, 7, 21): [_flowerYellow],
+      DateTime.utc(2023, 7, 22): [_flowerYellow],
+      DateTime.utc(2023, 7, 23): [_flowerYellow],
+      DateTime.utc(2023, 7, 25): [_flowerGreen],
+      DateTime.utc(2023, 7, 28): [_flowerPurple],
+      DateTime.utc(2023, 7, 31): [_flowerGreen],
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +65,15 @@ class TableCalendarScreen extends StatelessWidget {
           outsideDaysVisible: true,
           weekendTextStyle: const TextStyle().copyWith(color: Colors.red),
           holidayTextStyle: const TextStyle().copyWith(color: Colors.blue[800]),
+        ),
+        calendarBuilders: CalendarBuilders(
+          markerBuilder: (context, date, events) {
+            final children = <Widget>[];
+            if (_markers.containsKey(date)) {
+              children.addAll(_markers[date]!);
+            }
+            return children.isNotEmpty ? Positioned(bottom: 1, child: Row(children: children)) : SizedBox.shrink();
+          },
         ),
       ),
     );
