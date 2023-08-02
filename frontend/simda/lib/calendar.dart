0cfca0a7312d6d96dc64ed09d014dbce26ae1a66
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-08-02 14:26:32 +0900

    달력 holiday 수정

--- frontend/simda/lib/calendar.dart
+++ frontend/simda/lib/calendar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+
 class TableCalendarScreen extends StatelessWidget {
   const TableCalendarScreen({Key? key}) : super(key: key);
 
@@ -8,14 +9,18 @@ class TableCalendarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: TableCalendar(
+        locale: 'ko_KR',
         firstDay: DateTime.utc(2021, 10, 16),
         lastDay: DateTime.utc(2030, 3, 14),
         focusedDay: DateTime.now(),
-
         headerStyle: const HeaderStyle(
           titleCentered: true,
-          formatButtonVisible : false,
-
+          formatButtonVisible: false,
+        ),
+        calendarStyle: CalendarStyle(
+          outsideDaysVisible: true,
+          weekendTextStyle: const TextStyle().copyWith(color: Colors.red),
+          holidayTextStyle: const TextStyle().copyWith(color: Colors.blue[800]),
         ),
       ),
     );
--- frontend/simda/lib/main.dart
+++ frontend/simda/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'main_page.dart';
+// 달력 한국 시각
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(
     nativeAppKey: '57f9375c3d4e8452f5facd24db42ff6b',
   );
+
+  // 달력 한국 시각
+  await initializeDateFormatting();
+
   runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyApp()));
