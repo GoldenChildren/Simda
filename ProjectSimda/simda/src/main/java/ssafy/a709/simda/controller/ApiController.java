7c57d616be11f7466549b1f0ce903f9c1a7cbecd
YS <dbtjq1592000@gmail.com>
2023-08-03 09:28:44 +0900

    Bard API Java version

--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/controller/ApiController.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/controller/ApiController.java
@@ -1,5 +1,8 @@
 package ssafy.a709.simda.controller;
 
+import com.pkslow.ai.AIClient;
+import com.pkslow.ai.GoogleBardClient;
+import com.pkslow.ai.domain.Answer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,31 +27,36 @@ public class ApiController {
 //            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\SSAFY\\AppData\\Local\\Programs\\Python\\Python311\\python",
 //                    "C:\\Users\\SSAFY\\Desktop\\Project\\S09P12A709\\ProjectSimda\\simda\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
 //                    content);
-
+//
             // test 2 - 경로찾기로 실행
 //            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\SSAFY\\AppData\\Local\\Programs\\Python\\Python311\\python",
 //                    System.getProperty("user.dir")+"\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
 //                    content);
 //            System.out.println(System.getProperty("user.dir"));
+//
+//            // test 3 - 돚거에서 실행
+//            ProcessBuilder pb = new ProcessBuilder("/usr/bin/python3.9",
+//                    System.getProperty("user.dir")+"\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
+//                    content);
+//            System.out.println(System.getProperty("user.dir")+"/api/bard.py");
+//
+//            Process p = pb.start();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
+//
+//            // 출력 값을 확인해 보는 코드
+//            String line = " ";
+//            String output = "";
+//            while (line != null) {
+//                output += line;
+//                line = br.readLine();
+//                output += "\n";
+//            }
 
-            // test 3 - 돚거에서 실행
-            ProcessBuilder pb = new ProcessBuilder("/usr/bin/python3.9",
-                    System.getProperty("user.dir")+"\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
-                    content);
-            System.out.println(System.getProperty("user.dir")+"\\api\\bard.py");
-
-            Process p = pb.start();
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
-
-            // 출력 값을 확인해 보는 코드
-            String line = " ";
-            String output = "";
-            while (line != null) {
-                output += line;
-                line = br.readLine();
-                output += "\n";
-            }
+            AIClient client = new GoogleBardClient("Ygj5yW4U7eHBq5WAD5CPYQlzJ-Bi0nrNSdAkri99eP1VIqXc4gzGainsORoV0sgLpsolPw.");
+            Answer answer = client.ask(content+"(이)라는 문장은 다음 보기 중 어디에 가장 가까워? 0 : 행복, 1 : 기쁨, 2 : 평온, 3 : 화남, 4 : 슬픔. 대답은 다음과 같은 형식으로만 대답해. ex) 답 : 1");
+            String output = answer.getChosenAnswer();
             System.out.println(output);
+
             // 0 : 행복, 1 : 기쁨, 2 : 평온, 3 : 화남, 4 : 슬픔
             if (output.contains("0")) {
                 System.out.println("hello, 0 행복");
