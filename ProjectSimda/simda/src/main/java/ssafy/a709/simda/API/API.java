f1e7229cb5f20909078a51d02978489acaa2123e
YS <dbtjq1592000@gmail.com>
2023-08-04 13:44:00 +0900

    API -> 이미지 캡셔닝 + 피드 질문 수정

--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/API/API.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/API/API.java
@@ -22,34 +22,6 @@ public class API {
         // 게시글의 Content를 받아서 Ptyhon의 Bard API로 정보를 전달한다
         int emotion = 0; // 감정 기본 값은 0으로 고정
         try {
-            // test 1 - 절대 경로 실행
-//            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\SSAFY\\AppData\\Local\\Programs\\Python\\Python311\\python",
-//                    "C:\\Users\\SSAFY\\Desktop\\Project\\S09P12A709\\ProjectSimda\\simda\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
-//                    content);
-//
-            // test 2 - 경로찾기로 실행
-//            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\SSAFY\\AppData\\Local\\Programs\\Python\\Python311\\python",
-//                    System.getProperty("user.dir")+"\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
-//                    content);
-//            System.out.println(System.getProperty("user.dir"));
-//
-//            // test 3 - 돚거에서 실행
-//            ProcessBuilder pb = new ProcessBuilder("/usr/bin/python3.9",
-//                    System.getProperty("user.dir")+"\\src\\main\\java\\ssafy\\a709\\simda\\api\\bard.py",
-//                    content);
-//            System.out.println(System.getProperty("user.dir")+"/api/bard.py");
-//
-//            Process p = pb.start();
-//            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
-//
-//            // 출력 값을 확인해 보는 코드
-//            String line = " ";
-//            String output = "";
-//            while (line != null) {
-//                output += line;
-//                line = br.readLine();
-//                output += "\n";
-//            }
             System.out.println(content);
             content = content.replaceAll("(\r\n|\r|\n|\n\r)", " ");
             AIClient client = new GoogleBardClient("Ygj5yW4U7eHBq5WAD5CPYQlzJ-Bi0nrNSdAkri99eP1VIqXc4gzGainsORoV0sgLpsolPw.");
@@ -58,6 +30,7 @@ public class API {
             System.out.println(output);
 
             // 0 : 행복, 1 : 기쁨, 2 : 평온, 3 : 화남, 4 : 슬픔
+
             if (output.contains("0") || output.contains("행복")) {
                 System.out.println("hello, 0 행복");
             } else if (output.contains("1") || output.contains("기쁨")) {
