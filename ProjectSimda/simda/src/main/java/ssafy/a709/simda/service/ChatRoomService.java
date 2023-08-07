3113623c2c4fd3195e1eadbdc321be38bdd213d6
KimByeongChan <qudcks8749@naver.com>
2023-08-07 11:09:29 +0900

    채팅 빌드 오류수정 중 ...

--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/service/ChatRoomService.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/service/ChatRoomService.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public interface ChatRoomService {
     //해당 유저의 채팅방 목록 가져오기
-    public List<ChatRoomDto> selectAllChatRoomList(int userId);
+//    public List<ChatRoomDto> selectAllChatRoomList(int userId);
 
     //해당 유저가 채팅방 목록 생성하기
-    public int createChatRoom(int user1Id, int user2Id);
+//    public int createChatRoom(int user1Id, int user2Id);
 }
--- ProjectSimda/simda/src/main/java/ssafy/a709/simda/service/ChatRoomServiceImpl.java
+++ ProjectSimda/simda/src/main/java/ssafy/a709/simda/service/ChatRoomServiceImpl.java
@@ -17,42 +17,42 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatRoomServiceImpl implements ChatRoomService{
 
-    @Autowired
-    private final ChatRoomRepository chatRoomRepository;
-
-    //채팅방 목록가져오기
-    @Override
-    public List<ChatRoomDto> selectAllChatRoomList(int userId) {
-        List<ChatRoomDto> responseList = new ArrayList<>();
-        List<Chatroom> resultList = chatRoomRepository.findByUser1IdOrUser2Id(userId, userId);
-        for (Chatroom chatRoom:resultList) {
-            responseList.add(ChatRoomDto.changeToChatRoomDto(chatRoom));
-        }
-        return responseList;
-
-    }
-    //채팅방생성
-    @Override
-    public int createChatRoom(int user1Id, int user2Id) {
-        try {
-            UserDto user1 = new UserDto();
-            user1.setUserId(user1Id);
-            UserDto user2 = new UserDto();
-            user2.setUserId(user2Id);
-
-            Chatroom newChatroom = Chatroom.builder()
-                    .user1(User.changeToUser(user1))
-                    .user2(User.changeToUser(user2))
-                    .build();
-
-            chatRoomRepository.save(newChatroom);
-        }catch (Exception e){
-            //실패!
-            return 0;
-        }
-        //성공
-        return 1;
-    }
+//    @Autowired
+//    private final ChatRoomRepository chatRoomRepository;
+//
+//    //채팅방 목록가져오기
+//    @Override
+//    public List<ChatRoomDto> selectAllChatRoomList(int userId) {
+//        List<ChatRoomDto> responseList = new ArrayList<>();
+////        List<Chatroom> resultList = chatRoomRepository.findByUser1IdOrUser2Id(userId, userId);
+////        for (Chatroom chatRoom:resultList) {
+////            responseList.add(ChatRoomDto.changeToChatRoomDto(chatRoom));
+////        }
+//        return responseList;
+//
+//    }
+//    //채팅방생성
+//    @Override
+//    public int createChatRoom(int user1Id, int user2Id) {
+//        try {
+//            UserDto user1 = new UserDto();
+//            user1.setUserId(user1Id);
+//            UserDto user2 = new UserDto();
+//            user2.setUserId(user2Id);
+//
+//            Chatroom newChatroom = Chatroom.builder()
+//                    .user1(User.changeToUser(user1))
+//                    .user2(User.changeToUser(user2))
+//                    .build();
+//
+//            chatRoomRepository.save(newChatroom);
+//        }catch (Exception e){
+//            //실패!
+//            return 0;
+//        }
+//        //성공
+//        return 1;
+//    }
 
 
 
