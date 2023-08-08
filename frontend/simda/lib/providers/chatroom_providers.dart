9c50da315386cf48f3b15fb29fa93fabb7b2ae0f
KimByeongChan <qudcks8749@naver.com>
2023-08-08 12:30:41 +0900

    채팅dto 및 채팅 목록 구현

--- frontend/simda/lib/providers/chatroom_providers.dart
+++ frontend/simda/lib/providers/chatroom_providers.dart
@@ -1,22 +1,22 @@
 import 'dart:convert';
 
 import 'package:simda/models/ChatRoomDto.dart';
-import 'package:simda/models/UserDto.dart';
 import 'package:http/http.dart' as http;
 
 class ChatRoomProviders {
-  Future<List<ChatRoomDto>> getChatRoom(UserDto user) async {
+  Future<List<ChatRoomDto>> getChatRoom(int userId) async {
     List<ChatRoomDto> chatroom = [];
     String uri = "http://i9a709.p.ssafy.io:8000/simda/chat/rooms/";
-    uri += "${user.userId}";
+    uri += "${userId}";
 
     final response = await http.get(Uri.parse(uri));
 
     if (response.statusCode == 200) {
       chatroom =
-          jsonDecode(response.body)['chatrooms'].map<ChatRoomDto>((chatrooms) {
+          jsonDecode(response.body).map<ChatRoomDto>((chatrooms) {
         return ChatRoomDto.fromJson(chatrooms);
       }).toList();
+      print(chatroom);
     }
 
     return chatroom;
