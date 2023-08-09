15a25387ec1306d01697f982c2eb97a0732c11cf
jht043 <jht043@naver.com>
2023-08-09 14:16:32 +0900

    08/09 위치 권한 삽입

--- frontend/simda/lib/main.dart
+++ frontend/simda/lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           canvasColor: Colors.transparent,
         ),
-        home: viewModel.isLoggedIn == 1 ? MainPage(0) : const LoginPage(),
+        home: MainPage(0) ,
         // home: const MainPage(),
       ),
     );
--- frontend/simda/lib/main_page.dart
+++ frontend/simda/lib/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'main.dart';
 import 'map_page.dart';
@@ -39,6 +41,7 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _permission();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -50,6 +53,39 @@ class MainPage extends StatelessWidget {
         index: visit,
       ),
     );
+  }
+  void _permission() async {
+    var requestStatus = await Permission.location.request();
+    var status = await Permission.location.status;
+    print(requestStatus.isGranted);
+    print(status.isLimited);
+    if (requestStatus.isGranted && status.isLimited) {
+      // isLimited - 제한적 동의 (ios 14 < )
+      // 요청 동의됨
+      print("isGranted");
+      if (await Permission.locationWhenInUse.serviceStatus.isEnabled) {
+        // 요청 동의 + gps 켜짐
+        var position = await Geolocator.getCurrentPosition();
+        print("serviceStatusIsEnabled position = ${position.toString()}");
+      } else {
+        // 요청 동의 + gps 꺼짐
+        print("serviceStatusIsDisabled");
+      }
+    } else if (requestStatus.isPermanentlyDenied ||
+        status.isPermanentlyDenied) {
+      // 권한 요청 거부, 해당 권한에 대한 요청에 대해 다시 묻지 않음 선택하여 설정화면에서 변경해야함. android
+      print("isPermanentlyDenied");
+      openAppSettings();
+    } else if (status.isRestricted) {
+      // 권한 요청 거부, 해당 권한에 대한 요청을 표시하지 않도록 선택하여 설정화면에서 변경해야함. ios
+      print("isRestricted");
+      openAppSettings();
+    } else if (status.isDenied) {
+      // 권한 요청 거절
+      print("isDenied");
+    }
+    print("requestStatus ${requestStatus.name}");
+    print("status ${status.name}");
   }
 }
 
@@ -65,7 +101,6 @@ class BottomNavigationBar extends StatefulWidget {
 }
 
 class _BottomNavigationBarState extends State<BottomNavigationBar> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
